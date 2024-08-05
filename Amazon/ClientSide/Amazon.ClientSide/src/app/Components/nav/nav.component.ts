ed9bb032d831ccea662720c96b5646f81af2e622
Eweaa <88718116+Eweaa@users.noreply.github.com>
2024-08-05 21:12:03 +0300

    Created Some Components

--- Amazon/ClientSide/Amazon.ClientSide/src/app/Components/nav/nav.component.ts
+++ Amazon/ClientSide/Amazon.ClientSide/src/app/Components/nav/nav.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.css'
 })
 export class NavComponent {
+  constructor(private router:Router){}
 
+  query: string;
+
+  search = () => {
+    this.query = (<HTMLInputElement>document.getElementById("search"))?.value;
+    this.router.navigateByUrl(`/search/${this.query}`);
+    console.log(window.location.href)
+  }
 }
