880dca6699a8e3e50872e2e5ef58697cab12ecd9
Abdeen ElRefaey <MagedElRefaey777@gmail.com>
2024-10-02 21:41:38 +0300

    Category EndPoints And GenericRepo Creation

---
+++ Amazon/ServerSide/Amazon.Solution/Amazon.Core/Configurations/CategoryConfigurations.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Amazon.Core.Entities;
+namespace Amazon.Core.Configurations
+{
+    public class CategoryConfigurations
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            builder.Property(p => p.Id).IsRequired();
+            builder.Property(p => p.Name).IsRequired().HasMaxLength(100);
+            builder.Property(p => p.Description).IsRequired().HasMaxLength(100);
+            builder.Property(p => p.DisplayOrder).IsRequired();
+            builder.Property(p => p.IsActive).IsRequired();
+            builder.Property(p=>p.IsVisible).IsRequired();
+
+            builder.HasOne(p => p.ParentCategory)
+                    .WithMany(c => c.Categories)
+                    .HasForeignKey(p => p.ParentCategoryId);
+        }
+    }
+}
