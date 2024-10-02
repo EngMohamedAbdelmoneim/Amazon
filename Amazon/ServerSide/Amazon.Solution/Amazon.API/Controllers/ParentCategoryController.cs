a8747ae07eaba8bfaf2767988018c38f36f40133
Abdeen ElRefaey <MagedElRefaey777@gmail.com>
2024-10-02 21:35:30 +0300

    ParentCategory EndPoints And GenericRepo Creation

---
+++ Amazon/ServerSide/Amazon.Solution/Amazon.API/Controllers/ParentCategoryController.cs
@@ -0,0 +1,39 @@
+﻿using Amazon.Services.ParentCategoryService;
+using Amazon.Services.ParentCategoryService.Dto;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Amazon.API.Controllers
+{
+    public class ParentCategoryController : BaseController
+    {
+        private readonly IParentCategoryService _parentCategoryService;
+
+        public ParentCategoryController(IParentCategoryService parentCategoryService)
+        {
+            _parentCategoryService = parentCategoryService;
+        }
+
+        [HttpGet]
+        public async Task<IReadOnlyList<ParentCategoryToReturnDto>> GetAllParentCategories()
+             => await _parentCategoryService.GetAllParentCategoriesAsync();
+
+        [HttpPost]
+        public async Task<ParentCategoryToReturnDto> AddParentCategory(ParentCategoryDto parentCategory)
+            => await _parentCategoryService.AddParentCategory(parentCategory);
+
+        [HttpDelete("{Id}")]
+        public async Task<IReadOnlyList<ParentCategoryToReturnDto>> DeleteParentCategory(int Id)
+            =>await _parentCategoryService.DeleteParentCategory(Id);
+
+        [HttpPut("id")]
+        public async Task<ParentCategoryToReturnDto> UpdateProduct (int Id, ParentCategoryDto parentCategory)
+            => await _parentCategoryService.UpdateParentCategory(Id, parentCategory);
+
+        [HttpGet("{Id}")]
+        public async Task<ParentCategoryToReturnDto> GetProductById(int? Id)
+            => await _parentCategoryService.GetParentCategoryByIdAsync(Id);
+
+
+
+    }
+}
