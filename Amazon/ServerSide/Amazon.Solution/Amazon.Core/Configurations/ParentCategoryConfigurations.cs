46426df61b1ae890ccc28ff4faffb9329b234aac
Abdeen ElRefaey <MagedElRefaey777@gmail.com>
2024-10-02 21:42:12 +0300

    ParentCategory EndPoints And GenericRepo Creation

---
+++ Amazon/ServerSide/Amazon.Solution/Amazon.Core/Configurations/ParentCategoryConfigurations.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Amazon.Core.Entities;
+namespace Amazon.Core.Configurations
+{
+    public class ParentCategoryConfigurations
+    {
+        public void Configure(EntityTypeBuilder<ParentCategory> builder){
+            builder.Property(p => p.Id).IsRequired();
+            builder.Property(p => p.Name).IsRequired();
+        }
+    }
+}
