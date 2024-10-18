b67b4b9d35b4831f90cabaacaa9a60a2b6cea8ae
Eweaa <88718116+Eweaa@users.noreply.github.com>
2024-10-18 20:04:16 +0300

    Fixing the Interceptor

--- Amazon/ClientSide/Amazon.ClientSide/src/app/interceptors/add-token.interceptor.ts
+++ Amazon/ClientSide/Amazon.ClientSide/src/app/interceptors/add-token.interceptor.ts
@@ -1,12 +1,17 @@
 import { HttpInterceptorFn } from '@angular/common/http';
 
 export const addTokenInterceptor: HttpInterceptorFn = (req, next) => {
-  let token = localStorage.getItem("token");
-  if(token)
-  {
-    req = req.clone({setHeaders:{
-      'Authorization': `Bearer ${token}`
-    }})
+  let isLocalStorageAvailable  = typeof localStorage !== 'undefined';
+  if(isLocalStorageAvailable)
+    {
+    let token = localStorage.getItem('token')
+
+    if(token)
+    {
+      req = req.clone({setHeaders:{
+        'Authorization': `Bearer ${token}`
+      }})
+    }
   }
   return next(req);
 };
