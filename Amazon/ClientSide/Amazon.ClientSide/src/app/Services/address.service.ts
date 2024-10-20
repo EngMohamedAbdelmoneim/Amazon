b4c708b8e755d363d4d48eff2e69c5bc5fa85ba3
MagedElRefaey <MagedElRefaey777@gmail.com>
2024-10-20 22:57:15 +0300

    Updating address service
    Fixing token in address service

--- Amazon/ClientSide/Amazon.ClientSide/src/app/Services/address.service.ts
+++ Amazon/ClientSide/Amazon.ClientSide/src/app/Services/address.service.ts
@@ -13,18 +13,10 @@ export class AddressService {
   constructor(private http:HttpClient) { }
 
   getSavedAddresses():Observable<Address[]>{
-    const headers = new HttpHeaders({
-      'Authorization':'bearer eyJhbGciOiJodHRwOi8vd3d3LnczLm9yZy8yMDAxLzA0L3htbGRzaWctbW9yZSNobWFjLXNoYTI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1lIjoiQWJkZWxSYWhtYW5TYWxlaCIsImh0dHA6Ly9zY2hlbWFzLnhtbHNvYXAub3JnL3dzLzIwMDUvMDUvaWRlbnRpdHkvY2xhaW1zL3NpZCI6ImVhOGY4Y2QzLWE2YTAtNGU4Ni1iYWM1LTc0YWJlZGY1OGNmOSIsImh0dHA6Ly9zY2hlbWFzLnhtbHNvYXAub3JnL3dzLzIwMDUvMDUvaWRlbnRpdHkvY2xhaW1zL2VtYWlsYWRkcmVzcyI6ImFkbWluQGdtYWlsLmNvbSIsImh0dHA6Ly9zY2hlbWFzLm1pY3Jvc29mdC5jb20vd3MvMjAwOC8wNi9pZGVudGl0eS9jbGFpbXMvcm9sZSI6IkFkbWluIiwiZXhwIjoxNzI5MzUzNzUyLCJpc3MiOiJodHRwczovL2xvY2FsaG9zdDo3MjgyIiwiYXVkIjoiU2VjdXJlZEFQSVVzZXJzIn0.ZGNFxsDfcLQt-4fbC6Nl_LCcZk6UMvzyoSNuYRqMyMk',
-      'Content-Type' :'application/json'
-    });
-    return this.http.get<Address[]>(`${this.apiUrl}/getAddresses`,{headers});
+    return this.http.get<Address[]>(`${this.apiUrl}/getAddresses`);
   }
   addAddresses(address:Address):Observable<Address>{
-    const headers = new HttpHeaders({
-      'Authorization':'bearer eyJhbGciOiJodHRwOi8vd3d3LnczLm9yZy8yMDAxLzA0L3htbGRzaWctbW9yZSNobWFjLXNoYTI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1lIjoiQWJkZWxSYWhtYW5TYWxlaCIsImh0dHA6Ly9zY2hlbWFzLnhtbHNvYXAub3JnL3dzLzIwMDUvMDUvaWRlbnRpdHkvY2xhaW1zL3NpZCI6ImVhOGY4Y2QzLWE2YTAtNGU4Ni1iYWM1LTc0YWJlZGY1OGNmOSIsImh0dHA6Ly9zY2hlbWFzLnhtbHNvYXAub3JnL3dzLzIwMDUvMDUvaWRlbnRpdHkvY2xhaW1zL2VtYWlsYWRkcmVzcyI6ImFkbWluQGdtYWlsLmNvbSIsImh0dHA6Ly9zY2hlbWFzLm1pY3Jvc29mdC5jb20vd3MvMjAwOC8wNi9pZGVudGl0eS9jbGFpbXMvcm9sZSI6IkFkbWluIiwiZXhwIjoxNzI5MzUzNzUyLCJpc3MiOiJodHRwczovL2xvY2FsaG9zdDo3MjgyIiwiYXVkIjoiU2VjdXJlZEFQSVVzZXJzIn0.ZGNFxsDfcLQt-4fbC6Nl_LCcZk6UMvzyoSNuYRqMyMk',
-      'Content-Type' :'application/json'
-    });
-    return this.http.post<Address>(`${this.apiUrl}/addAddress`,address,{headers});
+    return this.http.post<Address>(`${this.apiUrl}/addAddress`,address);
   }
   updateAddress(address): Observable<any> {
     return this.http.put<any>(`${this.apiUrlGetAddress}/${address.id}`, address);
